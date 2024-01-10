256ee888a00f68c9ef13c412fd04a5f9a20cbf30
jusswa-uwu <110708589+jusswa-uwu@users.noreply.github.com>
2024-01-10 19:06:53 +0800

    this is an update

--- MainFrame.java
+++ MainFrame.java
@@ -1,38 +1,41 @@
-import java.awt.Color;
-import java.awt.Dimension;
+import java.sql.Array;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class MainFrame extends JFrame {
+import javax.sql.rowset.spi.SyncFactory;
+public class MainFrame{
     final static int height = 600;
     final static  int width = height/2+100;
-    JPanel MainPanel = new JPanel();
+   /*  JPanel MainPanel = new JPanel();
     JButton button0 = new JButton();
     JButton button1 = new JButton();
     JButton button2 = new JButton();
-    JButton button3 = new JButton();
+    JButton button5 = new JButton();
     JButton button4 = new JButton();
     JButton button5 = new JButton();
     JButton button6 = new JButton();
     JButton button7 = new JButton();
     JButton button8 = new JButton();
     JButton button9 = new JButton();
+*/
+    public void bubblesorter(int[] arr) {
+        int n = arr.length;
+            for(int i = 0;i<n;i++) {
+                for(int j =i+1;j<n;j++) {
+                    if(arr[i] < arr[j]) {
+                        int temp;
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                 
+                }
+            }
+            System.out.println("Arranged Num");
+            for(int r:arr) {
+                 System.out.print(r);
+            }
+           
+}
 
-    MainFrame() {
-       
-       setSize(width,height);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        setLayout(null);
-        MainPanel.setBackground(Color.black);
-       
-        add(MainPanel);
 
-
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-    }
-   
+    
 }
--- Pr.java
+++ Pr.java
@@ -1,8 +1,9 @@
 public class Pr{
     public static void main(String[] args) {
-
-        new MainFrame();
-
+        int[] inArr = {1,2,3,4,5,6,7,8,9,0};
+       MainFrame MF = new MainFrame();
+        MF.bubblesorter(inArr);
+        
 
     }
 }
